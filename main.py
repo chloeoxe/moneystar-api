de61ad50069eaca5272b52dc71628992c48c496f
Titus Lowe <tituslowesocial@gmail.com>
2025-07-29 15:46:08 +0800

    Update transaction

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import date
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from crud import insert_transaction
+from crud import transaction_create, transaction_update
 from database import create_supabase_client
-from models import Transaction, TransactionCreate
+from models import Transaction, TransactionCreate, TransactionUpdate
 
 app = FastAPI()
 
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your Next.js dev server
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -35,8 +35,18 @@ async def get_transactions():
 @app.post("/transaction", response_model=Transaction)
 async def create_transaction(transaction: TransactionCreate):
     try:
-        res = insert_transaction(transaction)
+        res = transaction_create(transaction)
         return res
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        
+@app.put("/transaction/{transaction_id}", response_model=Transaction)
+async def update_transaction(transaction_id: str, transaction: TransactionUpdate):
+    try:
+       res = transaction_update(transaction_id, transaction)
+       return res
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
